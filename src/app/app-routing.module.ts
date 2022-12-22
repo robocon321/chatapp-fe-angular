a442d3a4322c8d458a3c5c9be1871864536b4cf9
robocon321 <robocon321n@gmail.com>
2022-12-22 23:54:58 +0700

    Signin, Signup, Notfound UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { SignInComponent } from './client/sign-in/sign-in.component';
+import { SignUpComponent } from './client/sign-up/sign-up.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './client/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '404', component: NotFoundComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: "**", redirectTo: "/404", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
