04468686ff54fa8cca0f8c69e97560ac4198b633
robocon321 <robocon321n@gmail.com>
2023-01-16 23:40:28 +0700

    UI manage blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { CreateBlogComponent } from './pages/admin/create-blog/create-blog.component';
-import { LoadingComponent } from './components/loading/loading.component';
-import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CreateBlogComponent } from './pages/admin/create-blog/create-blog.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +33,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    CKEditorModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/admin.module.ts
+++ src/app/modules/admin.module.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
   declarations: [DashboardComponent, BlogComponent],
   imports: [
     CommonModule,
-    AdminRoutes
+    AdminRoutes    
   ],
 })
 export class AdminModule { }
